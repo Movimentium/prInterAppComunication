571a56cb79b3c742ab6c277ff5ae2e2e7b051d20
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-06-16 03:31:53 +0200

    creadas las 2 apps

---
+++ iOSappLlamada/iOSappLlamada/LlamadaVC.swift
@@ -0,0 +1,30 @@
+//
+//  LlamadaVC.swift
+//  iOSappLlamada
+//
+//  Created by Miguel on 16/06/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import UIKit
+
+class LlamadaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOSappLlamadora/iOSappLlamadora/LlamadoraVC.swift
@@ -0,0 +1,31 @@
+//
+//  LlamadoraVC.swift
+//  iOSappLlamadora
+//
+//  Created by Miguel on 16/06/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import UIKit
+
+class LlamadoraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onBtnAbrirLaOtraApp() {
+        guard let appLlamadaURL = URL(string: "iOSappLlamada://") else {
+            print("No se ha podido abrir")
+            return
+        }
+        UIApplication.shared.open(appLlamadaURL, options: [:]) { (success) in
+            if success == false {
+                print("Se ha intendado abrir pero no se ha podido")
+            }
+        }
+    }
+     
+}
